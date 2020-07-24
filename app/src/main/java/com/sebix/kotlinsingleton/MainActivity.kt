2508d859736fdd0b0aec23533f8138482ddc3244
sebastianbocul <seba23495@gmail.com>
2020-07-24 23:30:58 +0200

    Created new project

---
+++ app/src/main/java/com/sebix/kotlinsingleton/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.sebix.kotlinsingleton
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
